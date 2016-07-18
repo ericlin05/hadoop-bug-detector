78312891dcb7339b27358992c34499b09efcbe7b
Eric Lin <ericlin@ericlin.me>
2016-07-18 15:46:41 +1000

    importing jira bug catch exception so that it will continue

--- bug-import.py
+++ bug-import.py
@@ -1,9 +1,18 @@
 from lib.ApacheJiraParser import ApacheJiraParser
+import pycurl
+from lib.Logger import Logger
 
 # for a in range(890,894):
 #     parser = ApacheJiraParser('SENTRY-' + str(a))
 #     parser.parse().write()
 
-for a in range(8000,14240):
+for a in range(9525,14240):
     parser = ApacheJiraParser('HIVE-' + str(a))
-    parser.parse().write()
+
+    logger = Logger(__name__)
+
+    try:
+        parser.parse().write()
+    except pycurl.error as err:
+        logger.error("Unable to parse JIRA: %s", 'HIVE-' + str(a))
+        logger.error("Error: %s", str(err))
